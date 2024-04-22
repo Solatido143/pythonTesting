2ae209323610590df65bfd7123d40ee2b7a6b167
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2024-04-22 15:47:55 +0800

    Guess the Number

--- test.py
+++ test.py
@@ -1,18 +1,49 @@
-print("1. Celsius to Fahrenheit Conversion\n2. Fahrenheit to Celsius Conversion\n3. Exit")
-conversion_type = int(input("Enter choice (1/2/3): "))
+import random
+def guess_the_number():
+    secret_number = random.randint(1, 100)
 
-if conversion_type == 1:
-    cel = float(input("Enter temperature in Celcius: "))
-    far = (cel * 9/5) + 32
-    print(str(cel) + "°C is equal to " + str(round(far)) + "°F")
-elif conversion_type == 2:
-    far = float(input("Enter temperature in Fahrenheit: "))
-    cel = (far - 32) * 5/9
-    print(str(far) + "°F is equal to " + str(round(cel)) + "°C")
-elif conversion_type == 3:
-    exit()
-else:
-    print("Error Conversion Type Input!!")
+    attempts = 0
+
+    print("Welcome to the Guess the Number game!")
+    print("I've chosen a number between 1 and 100. Can you guess it?")
+
+    while True:
+        try:
+            guess = int(input("Your Guess: "))
+            attempts += 1
+
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+guess_the_number()
+
+
+
+
+
+
+
+
+# print("1. Celsius to Fahrenheit Conversion\n2. Fahrenheit to Celsius Conversion\n3. Exit")
+# conversion_type = int(input("Enter choice (1/2/3): "))
+# if conversion_type == 1:
+#     cel = float(input("Enter temperature in Celcius: "))
+#     far = (cel * 9/5) + 32
+#     print(str(cel) + "°C is equal to " + str(round(far)) + "°F")
+# elif conversion_type == 2:
+#     far = float(input("Enter temperature in Fahrenheit: "))
+#     cel = (far - 32) * 5/9
+#     print(str(far) + "°F is equal to " + str(round(cel)) + "°C")
+# elif conversion_type == 3:
+#     exit()
+# else:
+#     print("Error Conversion Type Input!!")
 
 
 
